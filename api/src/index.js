0ecd7b94de3f485270ed46efafc86fdf2794cc60
Manoj <manoj.victor@westernacher.com>
2025-09-23 10:29:33 +0530

    add nodemailer integration and email sending endpoint

--- api/src/index.js
+++ api/src/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
 const { auth } = require("express-oauth2-jwt-bearer");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
@@ -12,10 +13,10 @@ app.use((req, res, next) => {
 });
 
 const allowedOrigins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+  "http://localhost:3000",
+  "http://127.0.0.1:3000",
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
   process.env.FRONTEND_URL,
 ];
 
@@ -39,6 +40,7 @@ app.options(/.*/, cors());
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 
 const checkJwt = auth({
   audience: process.env.AUTH0_AUDIENCE,
@@ -67,3 +69,31 @@ app.get("/api/private", checkJwt, (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.zoho.in",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "support@ioak.org",
+    pass: "qNWHvTRpJFWs",
+  },
+});
+
+app.post("/text-mail", (req, res) => {
+  const { to, subject, text } = req.body;
+  const mailData = {
+    from: "support@ioak.org",
+    to: to,
+    subject: subject,
+    text: text,
+    html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>",
+  };
+
+  transporter.sendMail(mailData, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    res.status(200).send({ message: "Mail send", message_id: info.messageId });
+  });
+});
